dbd83df20e9813c74c4d8fb57495a3bd3897f553
davidnamills <mullerlight73@hotmail.com>
2017-10-25 12:01:01 +0800

    extracted css into own file

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,58 @@
 const path = require('path');
+const ExtractTextPlugin= require('extract-text-webpack-plugin');
 const AbsolutePath = path.join(__dirname, 'public');
 
-module.exports={
-    entry:'./src/app.js',
-    // entry:'./src/playground/hoc.js',
-    output: {
-        path: AbsolutePath,
-        filename: 'bundle.js'
-    },
 
-    module:{
-        rules:[
-            {
-                loader: 'babel-loader',
-                test: /\.js$/,
-                exclude: /node_modules/
-            },
-            {
-                test: /\.s?css$/,
-                use: ['style-loader','css-loader', 'sass-loader']
-            }
-        ]
-    },
-    devtool: 'cheap-module-eval-source-map',
-    devServer:{
-        contentBase: AbsolutePath,
-        historyApiFallback: true
-    }
+module.exports=(env)=>{
+    const isProduction = env === 'production';
+    const CSSExtract = new ExtractTextPlugin('styles.css');
 
+    console.log(env);
+
+    return {
+        entry:'./src/app.js',
+        // entry:'./src/playground/hoc.js',
+        output: {
+            path: AbsolutePath,
+            filename: 'bundle.js'
+        },
+    
+        module:{
+            rules:[
+                {
+                    loader: 'babel-loader',
+                    test: /\.js$/,
+                    exclude: /node_modules/
+                },
+                {
+                    test: /\.s?css$/,
+                    use: CSSExtract.extract({
+                        use: [
+                            {
+                                loader: 'css-loader',
+                                options: {
+                                    sourceMap:true
+                                }
+                            },
+                            {
+                                loader:'sass-loader',
+                                options: {
+                                    sourceMap:true
+                                }
+                            }
+                        ]
+                    })
+                }
+            ]
+        },
+        plugins:[
+            CSSExtract
+        ],
+        devtool: isProduction? 'source-map': 'cheap-module-eval-source-map',
+        devServer:{
+            contentBase: AbsolutePath,
+            historyApiFallback: true
+        }
+    
+    };
 };
